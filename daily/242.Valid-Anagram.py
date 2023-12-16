6696e96d26e95d29a6259ae8518c7f21e5a92b24
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2023-12-16 18:49:27 +0200

    242. Valid Anagram

---
+++ daily/242.Valid-Anagram.py
@@ -0,0 +1,10 @@
+# 242. Valid Anagram
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return sorted(s) == sorted(t)
