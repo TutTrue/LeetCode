9ec2951012f7b76a8a5264597464fb5927f77c39
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-01-30 06:44:14 +0200

    [150] [Time Beats: 71.20%] [Memory Beats: 45.98%]

---
+++ 150-Evaluate-Reverse-Polish-Notation.py
@@ -0,0 +1,21 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        st = []
+        for token in tokens:
+            if token == \+\:
+                st.append(int(st.pop()) + int(st.pop()))
+            elif token == \*\:
+                st.append(int(st.pop()) * int(st.pop()))
+            elif token == \-\:
+                num1 = int(st.pop())
+                num2 = int(st.pop())
+                st.append(num2-num1)
+            elif token == \/\:
+                num1 = int(st.pop())
+                num2 = int(st.pop())
+                st.append(num2/num1)
+            else:
+                st.append(token)
+
+        return int(st.pop())
+        
