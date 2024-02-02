3970eeeba270898e3c827ff915153902b8b5fdfd
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-02-02 11:18:54 +0200

    [1291] [Time Beats: 63.35%] [Memory Beats: 18.32%] - LeetPush

---
+++ 1291-Sequential-Digits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        c = "123456789"
+        a = []
+
+        for i in range(len(c)):
+            for j in range(i + 1, len(c) + 1):
+                curr = int(c[i:j])
+                if low <= curr <= high:
+                    a.append(curr)
+
+        a.sort()
+        return a
