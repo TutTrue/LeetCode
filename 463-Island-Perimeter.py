44c40a3d05956fe9a004f26743bc6c493437d9b8
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-02-24 20:45:35 +0200

    [463] [Time Beats: 67.82%] [Memory Beats: 93.61%] - LeetPush

---
+++ 463-Island-Perimeter.py
@@ -0,0 +1,12 @@
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == 1:
+                    count+=4
+                    if i > 0 and grid[i - 1][j] == 1:
+                        count-=2
+                    if j > 0 and grid[i][j - 1] == 1:
+                        count-=2
+        return count
