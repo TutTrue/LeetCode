14eb9d7c193b5801a7c2029d0c874d038e5086e4
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-03-01 22:56:45 +0200

    [2864] [Time Beats: 75.08%] [Memory Beats: 44.53%] - LeetPush

---
+++ 2864-Maximum-Odd-Binary-Number.py
@@ -0,0 +1,5 @@
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        ones = s.count("1")
+        zeros = len(s) - ones
+        return '1' * (ones - 1) + '0' * zeros + '1'
