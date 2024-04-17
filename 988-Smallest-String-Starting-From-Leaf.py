b6cfcb551a0399716fef69dfd861b4a35e6239cd
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-04-17 13:08:04 +0200

    [988] [Time Beats: 58.54%] [Memory Beats: 88.96%] - LeetPush

--- 988-Smallest-String-Starting-From-Leaf.py
+++ 988-Smallest-String-Starting-From-Leaf.py
@@ -7,14 +7,19 @@
 class Solution:
     def smallestFromLeaf(self, root: Optional[TreeNode]) -> str:
         res = []
+        m = None
         def dfs(root=root, s=''):
+            nonlocal m
             if not root:
                 return
             if not root.left and not root.right:
                 st = s+chr(root.val + 97)
-                res.append(st[::-1])
+                if not m:
+                    m = st[::-1]
+                else:
+                    m = min(m, st[::-1])
                 return
             dfs(root.left, s+chr(root.val + 97))
             dfs(root.right, s+chr(root.val + 97))
         dfs()
-        return min(res)
+        return m
