5bbb85a60271ac55cc983412f8dca0c8e219447a
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-04-26 09:35:42 +0300

    [1289] [Time Beats: 27.87%] [Memory Beats: 92.04%] - LeetPush

--- 1289-Minimum-Falling-Path-Sum-II.py
+++ 1289-Minimum-Falling-Path-Sum-II.py
@@ -8,19 +8,3 @@ class Solution:
                         m = min(m, grid[r-1][i])
                 grid[r][c] += m
         return min(grid[-1])
-        # m = float('inf')
-        # memo = {}
-        # def rec(path=0, prev=(-1, 0)):
-        #     nonlocal m
-        #     if prev in memo:
-        #         return
-        #     if prev[1] == len(grid):
-        #         m = min(m, path)
-        #         memo[prev] = m
-        #         return
-
-        #     for i in range(len(grid[prev[1]])):
-        #         if i != prev[0]:
-        #             rec(path+grid[prev[1]][i], (i, prev[1]+1))
-        # rec()
-        # return m
