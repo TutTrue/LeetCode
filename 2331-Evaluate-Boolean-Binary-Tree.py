93105e92704bcd1ff8e3bd0625531031215cef62
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-05-16 10:30:49 +0300

    [2331] [Time Beats: 37.05%] [Memory Beats: 92.02%] - LeetPush

---
+++ 2331-Evaluate-Boolean-Binary-Tree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def evaluateTree(self, root: Optional[TreeNode]) -> bool:
+        if not root.left and not root.right:
+            return root.val
+        return (
+            (self.evaluateTree(root.right) | self.evaluateTree(root.left))
+            if root.val == 2
+            else (self.evaluateTree(root.right) & self.evaluateTree(root.left))
+        )
