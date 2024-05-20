b032939062256bcb3cf7eff904fda97dc7198223
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-05-20 04:14:06 +0300

    [1863] [Time Beats: 79.90%] [Memory Beats: 84.04%] - LeetPush

--- 1863-Sum-of-All-Subset-XOR-Totals.py
+++ 1863-Sum-of-All-Subset-XOR-Totals.py
@@ -1,12 +1,12 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         total = 0
-        def rec(i=0, arr=[]):
+        def rec(i=0, cur=0):
             nonlocal total
             if i == len(nums):
-                total += reduce(xor, arr, 0)
+                total+=cur
                 return
-            rec(i+1, arr)
-            rec(i+1, arr+[nums[i]])
+            rec(i+1, cur)
+            rec(i+1, cur^nums[i])
         rec()
         return total
