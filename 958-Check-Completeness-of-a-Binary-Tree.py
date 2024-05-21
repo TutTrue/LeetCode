4e85216b212329b113b267c38aaee13678a4f749
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-05-21 12:54:35 +0300

    [958] [Time Beats: 27.84%] [Memory Beats: 92.61%] - LeetPush

--- 958-Check-Completeness-of-a-Binary-Tree.py
+++ 958-Check-Completeness-of-a-Binary-Tree.py
@@ -7,21 +7,10 @@
 class Solution:
     def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
         bfs = deque([root])
-        res = [root.val]
-        while bfs:
+        while bfs[0]:
             tmp = bfs.popleft()
-            if tmp.left:
-                bfs.append(tmp.left)
-            if tmp.right:
-                bfs.append(tmp.right)
-            res.append(tmp.left.val if tmp.left else None)
-            res.append(tmp.right.val if tmp.right else None)
-        j = len(res) - 1
-        while not res[j]:
-            j-=1
-        for i in range(j):
-            if not res[i]:
-                return False
-        print(res)
-        return True
-        
+            bfs.append(tmp.left)
+            bfs.append(tmp.right)
+        while bfs and not bfs[0]:
+            bfs.popleft()
+        return not bfs # return True if len(bfs) == 0 else False
