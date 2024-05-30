a2a2ba3d4c07d0c963aba5f2dbb47ba822ded5e9
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-05-30 03:28:20 +0300

    [1442] [Time Beats: 57.47%] [Memory Beats: 93.68%] - LeetPush

---
+++ 1442-Count-Triplets-That-Can-Form-Two-Arrays-of-Equal-XOR.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countTriplets(self, arr: List[int]) -> int:
+        a, b = 0, 0
+        count = 0
+        for i in range(len(arr)):
+            a = 0
+            for j in range(i, len(arr)):
+                a ^= arr[j]
+                if a == 0:
+                    count+=j - i
+        return count
