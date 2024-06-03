680868c89a17244de0d4c333819689d2ba4f6fe3
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-06-03 16:43:00 +0300

    [987] [Time Beats: 37.31%] [Memory Beats: 94.95%] - LeetPush

--- 987-Vertical-Order-Traversal-of-a-Binary-Tree.py
+++ 987-Vertical-Order-Traversal-of-a-Binary-Tree.py
@@ -1,13 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        n = len(deck)
-        deck.sort()
-        q = deque(range(n))
-        res = [0] * n
-
-        for card in deck:
-            res[q.popleft()] = card
-            if q:
-                q.append(q.popleft())
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        dic = defaultdict(list)
+        def dfs(root=root, index=(0, 0)):
+            if not root:
+                return 
+            dic[index[1]].append((index[0], root.val))
+            dfs(root.left, (index[0] + 1, index[1] - 1))
+            dfs(root.right, (index[0] + 1, index[1] + 1))
+        dfs()
+        res=[]
+        for k, v in sorted(dic.items()):
+            v.sort()
+            res.append([x[1] for x in v])
 
         return res
