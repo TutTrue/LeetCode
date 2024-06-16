f51fb73f4bb8731c2890cd58cf99132e42c35130
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-06-16 15:40:40 +0300

    [330] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

---
+++ DCP-06-24/330-Patching-Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        ans = 0
+        sum_val = 1
+        m = len(nums)
+        i = 0
+
+        while sum_val <= n:
+            if i < m and nums[i] <= sum_val:
+                sum_val += nums[i]
+                i += 1
+            else:
+                sum_val += sum_val
+                ans += 1
+        
+        return ans
