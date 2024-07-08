5ba978ae1d63ff371868d6a5451b54f3c3964e71
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-07-08 13:06:28 +0300

    [1823] [Time Beats: 51.02%] [Memory Beats: 49.47%] - LeetPush

---
+++ DCP-07-24/1823-Find-the-Winner-of-the-Circular-Game.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        arr = [i for i in range(1, n+1)]
+        i = 0
+        while len(arr) != 1:
+            i = (i + k - 1) % len(arr)
+            arr.pop(i)
+        return arr[0]
