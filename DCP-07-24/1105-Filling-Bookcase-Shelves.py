e07e305940a549c73bd9964a50234000e11c0fe8
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-07-31 10:09:57 +0300

    [1105] [Time Beats: 48.02%] [Memory Beats: 59.89%] - LeetPush

---
+++ DCP-07-24/1105-Filling-Bookcase-Shelves.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
+        dp = [0] * (len(books) + 1)
+        for i, (w, h) in enumerate(books, 1):
+            dp[i] = dp[i-1] + h
+            for j in range(i-1, 0, -1):
+                w += books[j-1][0]
+
+                if w > shelfWidth:
+                    break
+                
+                h = max(h, books[j-1][1])
+                dp[i] = min(dp[i], dp[j-1]+h)
+        return dp[-1]
