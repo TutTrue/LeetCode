753150592092910d2e8e2151d3e6d617304b7d98
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-08-02 13:55:26 +0300

    [2134] [Time Beats: 96.40%] [Memory Beats: 37.84%] - LeetPush

---
+++ DCP-08-24/2134-Minimum-Swaps-to-Group-All-1's-Together-II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minSwaps(self, nums: List[int]) -> int:
+        n = len(nums)
+        total_ones = sum(nums)
+        
+        if total_ones == 0 or total_ones == n:
+            return 0
+        
+        current_ones = sum(nums[:total_ones])
+        max_ones = current_ones
+        
+        for i in range(n):
+            current_ones -= nums[i]
+            current_ones += nums[(i + total_ones) % n]
+            max_ones = max(max_ones, current_ones)
+        
+        return total_ones - max_ones
