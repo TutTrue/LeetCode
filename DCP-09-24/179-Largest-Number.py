742afa9d84456031c950cd980105a6271d46e438
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-09-18 09:46:42 +0300

    [179] [Time Beats: 87.65%] [Memory Beats: 32.99%] - LeetPush

---
+++ DCP-09-24/179-Largest-Number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i in range(len(nums)):
+            nums[i] = str(nums[i])
+
+        nums.sort(key=lambda x: x*10, reverse=True)
+        if nums[0] == \0\:
+            return \0\
+        
+        return ''.join(nums)
