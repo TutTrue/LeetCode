6a855218d3512c75fedbdeb3bbabda99a37e3302
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2024-12-02 10:15:34 +0200

    [1455] [Time Beats: 100.00%] [Memory Beats: 7.12%] - LeetPush

---
+++ DCP-12-24/1455-Check-If-a-Word-Occurs-As-a-Prefix-of-Any-Word-in-a-Sentence.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+        for i, w in enumerate(sentence.split()):
+            if searchWord == w[:len(searchWord)]:
+                return i + 1
+
+        return -1
