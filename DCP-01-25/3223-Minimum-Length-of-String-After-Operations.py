26a1d4c958bf9f08d87d85083a3a739ccb6c8625
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2025-01-13 08:06:49 +0200

    [3223] [Time Beats: 69.84%] [Memory Beats: 61.90%] - LeetPush

--- DCP-01-25/3223-Minimum-Length-of-String-After-Operations.py
+++ DCP-01-25/3223-Minimum-Length-of-String-After-Operations.py
@@ -2,7 +2,7 @@ class Solution:
     def minimumLength(self, s: str) -> int:
         freq = Counter(s)
         res = 0
-        for k, v in freq.items():
+        for v in freq.values():
             res += 1 if v % 2 else 2
 
         return res
