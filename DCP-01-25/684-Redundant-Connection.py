93b341450938d6971a6d0dbf4255dd09a3545f51
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2025-01-29 18:19:14 +0200

    [684] [Time Beats: 37.52%] [Memory Beats: 61.58%] - LeetPush

---
+++ DCP-01-25/684-Redundant-Connection.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        root = list(range(len(edges) + 1))
+
+        def find_root(node):
+            if root[node] != node:
+                root[node] = find_root(root[node])
+            return root[node]
+
+        for node1, node2 in edges: 
+            root1, root2 = find_root(node1), find_root(node2)
+
+            if root1 == root2:
+                return [node1, node2]
+ 
+            root[root2] = root1
+        return root
