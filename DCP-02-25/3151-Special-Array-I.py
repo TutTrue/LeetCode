36e05e1e3071dbc522803ca38762e40abf2be809
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2025-02-01 12:35:23 +0200

    [3151] [Time Beats: 100.00%] [Memory Beats: 33.33%] - LeetPush

---
+++ DCP-02-25/3151-Special-Array-I.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        if len(nums) == 1: return True
+        for i in range(len(nums) - 1):
+            if nums[i] % 2 == nums[i+1] % 2: return False
+
+        return True
