1ed60bc2e7fea9e327391d0670a47ee45f7c92cf
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2025-04-20 20:33:38 +0200

    [781] [Time Beats: 100.00%] [Memory Beats: 29.10%] - LeetPush

---
+++ DCP-04-25/Rabbits-in-Forest.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        freq = Counter(answers)
+        total = 0
+        for k, v in freq.items():
+            group_size = k + 1
+            groups = ceil(v / group_size)
+            total += groups * group_size
+        return total
