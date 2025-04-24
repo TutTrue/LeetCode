0371c6bff49b959c9d6d7982bd8fb125cbbe5f3a
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2025-04-24 09:06:53 +0200

    [2799] [Time Beats: 95.10%] [Memory Beats: 5.77%] - LeetPush

--- DCP-04-25/Count-Complete-Subarrays-in-an-Array.py
+++ DCP-04-25/Count-Complete-Subarrays-in-an-Array.py
@@ -1,11 +1,16 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
+        dic = defaultdict(int)
         n = len(nums)
         dis = len(set(nums))
         res = 0
-        for i in range(n):
-            s =set()
-            for j in range(i, n):
-                s.add(nums[j])
-                if len(s) == dis: res +=1
+        l = 0
+        for r in range(n):
+            dic[nums[r]] += 1
+            while len(dic) == dis:
+                res+= n - r
+                dic[nums[l]] -= 1
+                if dic[nums[l]] == 0:
+                    del dic[nums[l]]
+                l+=1
         return res
