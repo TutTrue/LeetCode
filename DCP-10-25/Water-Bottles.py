696a68c9ab94aa86889a2560ee23797c0278b842
Mahmoud Hamdy Abdelhady <106309885+TutTrue@users.noreply.github.com>
2025-10-02 00:39:12 +0300

    [1518] [Time Beats: 100.00%] [Memory Beats: 61.43%] - LeetPush

---
+++ DCP-10-25/Water-Bottles.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        empty = numBottles
+        total = numBottles
+        while empty >= numExchange:
+            new_bottles = empty // numExchange
+            total += new_bottles
+            empty = (empty % numExchange) + new_bottles
+        return total
+        
